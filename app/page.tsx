557f254f3de1d303c88c84843972a99ae1242e95
NV Rastogi <rastoginishant@yahoo.in>
2024-12-04 14:21:24 +0530

    Change The Entry Page As Register

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 import { Container } from "@mui/material";
-import Login from "./login/page";
+import Register from "./register/page";
 
 export default function Home() {
 	return (
 		<Container component='main' maxWidth='xs'>
-			<Login />
+			<Register />
 		</Container>
 	);
 }
